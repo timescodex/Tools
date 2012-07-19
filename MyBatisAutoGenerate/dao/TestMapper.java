98025f55451b6d231ceb409cae180e339e2e1627
yang <yang@yang-System-Product-Name.(none)>
2012-07-19 11:49:51 +0800

    mybatis generator

---
+++ MyBatisAutoGenerate/dao/TestMapper.java
@@ -0,0 +1,9 @@
+package /home/yang/Imeiya/Dev/Tools/Tools/MyBatisAutoGenerate/dao/;
+
+import /home/yang/Imeiya/Dev/Tools/Tools/MyBatisAutoGenerate/dao/.Test;
+
+public interface TestMapper {
+    int insert(Test record);
+
+    int insertSelective(Test record);
+}
